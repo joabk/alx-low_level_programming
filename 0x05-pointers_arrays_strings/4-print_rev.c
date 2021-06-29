3be16e07392317c2a5033ac31380db945a7644f6
Joab Karani <mjoab88@gmail.com>
2021-06-29 11:30:05 -0700

    Updated to betty coding standard

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,12 @@
  */
 void print_rev(char *s)
 {
-  int i = 0, c;
+int i = 0, c;
 
-  while (s[c] != '\0')
-    c++;
-  c--;
-  for (i = c; i >= 0; i--)
-    _putchar(s[i]);
-  _putchar('\n');
+while (s[c] != '\0')
+c++;
+c--;
+for (i = c; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,16 @@
  */
 void rev_string(char *s)
 {
-  int i, c = 0;
-  char temp;
+int i, c = 0;
+char temp;
 
-  while (s[c] != '\0')
-    c++;
-  c--;
-  for (i = 0; i < c; c--, i++)
-    {
-      temp = s[i];
-      s[i] = s[c];
-      s[c] = temp;
-    }
+while (s[c] != '\0')
+c++;
+c--;
+for (i = 0; i < c; c--, i++)
+{
+temp = s[i];
+s[i] = s[c];
+s[c] = temp;
+}
 }
